a63a141f88670ecc5d228272a15d4ac6d169ce66
Daniel Rivera <drivera@xentrl.com>
2017-10-06 22:03:13 -0700

    Fix Mongoose Package Name

--- server/app.js
+++ server/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 const demoPort = 3000;
+const mongoose = require("mongoose");
 
-app.listen(demoPort, function() {
+var dbOptions = {
+    useMongoClient: true,
+    autoReconnect: true,
+    keepAlive: 1,
+    connectTimeoutMS: 300000
+};
+
+mongoose.connect('mongodb://localhost/haptos    ', { useMongoClient: true });
+
+app.listen(demoPort, function () {
     console.log("app listen on " + demoPort);
 });
