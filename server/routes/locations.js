bd41e29969389b38e202a1b92a55422b18d10848
Samuel <evileumas@gmail.com>
2017-10-06 23:03:56 -0700

    Added endpoints for users

--- server/routes/locations.js
+++ server/routes/locations.js
@@ -6,4 +6,12 @@ module.exports = function (router) {
     router.get("/locations/:id", function (req, res) {
         return res.status(200).send({ message: "locations " + req.params.id });
     });
+
+    router.get("/locations/:id/votes", function (req, res) {
+        return res.status(200).send({ message: "votes for location " + req.params.id });
+    });
+
+    router.post("/locations/:id/votes", function (req, res) {
+        return res.status(200).send({ message: "vote applied for location " + req.params.id });
+    });
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
     router.get("/users", function (req, res) {
         return res.status(200).send({ message: "users" });
     });
+
+    router.get("/users/:id", function (req, res) {
+        return res.status(200).send({ message: "user " + req.params.id });
+    });
 }
