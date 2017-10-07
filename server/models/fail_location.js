275a1f6d7ae91d5b2007b64f6065f0b2b1c34534
Daniel Rivera <drivera@xentrl.com>
2017-10-06 22:27:37 -0700

    Updated FailLocationSchema

--- server/models/fail_location.js
+++ server/models/fail_location.js
@@ -1,7 +1,9 @@
 var mongoose = require("mongoose");
 
 var FailLocationSchema = new mongoose.Schema({
-    name: { type: String }
+    name: { type: String },
+    place: { type: mongoose.Types.ObjectId },
+    location: { type: mongoose.Types.ObjectId }
 });
 
 module.exports = mongoose.model("FailLocation", FailLocationSchema);
